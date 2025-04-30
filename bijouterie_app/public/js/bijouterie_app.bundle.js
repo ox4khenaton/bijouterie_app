b15e101503ba9459906ed0cb8610679ca29703e5
ox4khenaton <60277346+ox4khenaton@users.noreply.github.com>
2025-04-30 15:32:09 +0100

    Update bijouterie_app.bundle.js

--- bijouterie_app/public/js/bijouterie_app.bundle.js
+++ bijouterie_app/public/js/bijouterie_app.bundle.js
@@ -1,8 +1,3 @@
-// Main entry point for bijouterie_app bundle
-// This file is required for ERPNext v15 compatibility
-import "./bijouterie_app.js";
-import "./item.js";
-import "./barcode_mass_print.js";
-import "./pos_extensions.js";
-import "./algerian_market.js";
-import "./whitelabel.js";
+@import '/assets/bijouterie_app/css/bijouterie_app.css';
+@import '/assets/bijouterie_app/css/jewelry_theme.css';
+@import '/assets/bijouterie_app/css/algerian_jewelry_theme.css';
